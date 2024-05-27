419eef630b64ae19ee6ec90ebec1a8c1aef58602
arthells <jill.nova@bk.ru>
2024-05-27 16:22:36 +0300

    cfg admins update

--- Bot/Shop/handlers/users/handler_start.py
+++ Bot/Shop/handlers/users/handler_start.py
@@ -6,7 +6,7 @@ from Bot.Shop import keyboards
 from tools.logger import log
 
 
-@bot.message_handler(commands=['start.bat'])
+@bot.message_handler(commands=['start'])
 def start_handler(m: types.Message):
     reffer = ""
     users = dbase.get_users()
